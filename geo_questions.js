c42829fc17242abdf1763bb88562e31387231953
Marcky <marckybalabamlacc@gmail.com>
2026-03-13 21:24:06 +0800

    geo_topic_progress

---
+++ geo_questions.js
@@ -0,0 +1,46 @@
+
+//question element getter
+const js_question = document.getElementById('quest');
+const js_btn = document.getElementById("test");
+
+//list of questions
+const questions = [
+  question_1 = {
+    question: "What is the capital of Canada?",
+    choice_a: 'Beijing',
+    choice_b: 'Ottawa',
+    choice_c: 'Seoul',
+    choice_d: 'Manila'
+  },
+  question_2 = {
+    question: "What is the capital of Denmark?",
+    choice_a: 'Beijing',
+    choice_b: 'Ottawa',
+    choice_c: 'Seoul',
+    choice_d: 'Manila'
+  },
+  question_3 = {
+    question: "Which city is the largest not situated on a river, lake, or coastline?",
+    choice_a: 'Beijing',
+    choice_b: 'Ottawa',
+    choice_c: 'Seoul',
+    choice_d: 'Manila'
+  },
+  question_4 = {
+    question: "What is the largest island in the world?",
+    choice_a: 'Beijing',
+    choice_b: 'Ottawa',
+    choice_c: 'Seoul',
+    choice_d: 'Manila'
+  },
+  question_5 = {
+    question: `Which continent is known as the "Frozen Continent"?`,
+    choice_a: 'Beijing',
+    choice_b: 'Ottawa',
+    choice_c: 'Seoul',
+    choice_d: 'Manila'
+  }
+]
+
+
+
